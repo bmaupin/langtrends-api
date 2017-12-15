15c1e3a814c9bc3433266ed0f2df3e2272f72cad
bmaupin <bmaupin@users.noreply.github.com>
2017-12-15 13:52:56 -0500

    Get language names from Github

---
+++ src/site/Github.js
@@ -0,0 +1,23 @@
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+
+export default class Github {
+  static async getLangNames() {
+    const GITHUB_LANGUAGES_URL = 'https://github.com/search/advanced';
+
+    let dom = await JSDOM.fromURL(GITHUB_LANGUAGES_URL);
+    let langNames = [];
+
+    let select = dom.window.document.getElementById('search_language');
+    let optgroups = select.getElementsByTagName('optgroup');
+
+    for (let i = 0; i < optgroups.length; i++) {
+        let options = optgroups[i].getElementsByTagName('option');
+        for (let j = 0; j < options.length; j++) {
+            langNames.push(options[j].textContent);
+        }
+    }
+
+    return langNames;
+  }
+}
