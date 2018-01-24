dd6ed73d18e56b1066cccd02e3d507747a0c7a15
bmaupin <bmaupin@users.noreply.github.com>
2018-01-24 12:29:25 -0500

    Add functionality for setting Stackoverflow API key

--- server/src/__tests__/Stackoverflow.test.js
+++ server/src/__tests__/Stackoverflow.test.js
@@ -4,6 +4,12 @@ const Stackoverflow = require('../Stackoverflow');
 
 let stackoverflow = new Stackoverflow();
 
+beforeAll(() => {
+  if (process.env.hasOwnProperty('STACKOVERFLOW_API_KEY')) {
+    stackoverflow.apiKey = process.env.STACKOVERFLOW_API_KEY;
+  }
+});
+
 test('Test getScore', async () => {
   expect(await stackoverflow.getScore('JavaScript', Date.parse('2017-01-01'))).toBeGreaterThan(1000000);
 });
