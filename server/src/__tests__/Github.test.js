9ec27dc1e0e6ff40c9befc66c195c3dbc7bc08fa
bmaupin <bmaupin@users.noreply.github.com>
2018-01-25 13:08:32 -0500

    Add getScore for github

--- server/src/__tests__/Github.test.js
+++ server/src/__tests__/Github.test.js
@@ -2,6 +2,16 @@
 
 const Github = require('../Github');
 
-test('Test getting language names from Github', async () => {
-  expect(await Github.getLangNames()).toContain('JavaScript');
+test('Test getLangNames', async () => {
+  let langNames = await Github.getLangNames();
+  expect(langNames.length).toBeGreaterThan(100);
+  expect(langNames).toContain('JavaScript');
+});
+
+test('Test getScore', async () => {
+  let github = new Github();
+  if (process.env.hasOwnProperty('GITHUB_API_KEY')) {
+    github.apiKey = process.env.GITHUB_API_KEY;
+  }
+  expect(await github.getScore('JavaScript', new Date('2017-01-01'))).toBeGreaterThan(1000000);
 });
