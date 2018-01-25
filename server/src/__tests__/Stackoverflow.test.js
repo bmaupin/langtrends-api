c1ad6e5fe794724027873527555626f8145491cb
bmaupin <bmaupin@users.noreply.github.com>
2018-01-25 13:09:12 -0500

    Simplify test for stackoverflow to match github test

--- server/src/__tests__/Stackoverflow.test.js
+++ server/src/__tests__/Stackoverflow.test.js
@@ -2,14 +2,10 @@
 
 const Stackoverflow = require('../Stackoverflow');
 
-let stackoverflow = new Stackoverflow();
-
-beforeAll(() => {
+test('Test getScore', async () => {
+  let stackoverflow = new Stackoverflow();
   if (process.env.hasOwnProperty('STACKOVERFLOW_API_KEY')) {
     stackoverflow.apiKey = process.env.STACKOVERFLOW_API_KEY;
   }
-});
-
-test('Test getScore', async () => {
-  expect(await stackoverflow.getScore('JavaScript', Date.parse('2017-01-01'))).toBeGreaterThan(1000000);
+  expect(await stackoverflow.getScore('JavaScript', new Date('2017-01-01'))).toBeGreaterThan(1000000);
 });
