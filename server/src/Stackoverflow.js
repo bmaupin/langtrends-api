db863b602c87e87fd436246bd46b2ec92ae95b96
bmaupin <bmaupin@users.noreply.github.com>
2018-01-26 12:33:06 -0500

    Refactor shared code into CodingSite class

--- server/src/Stackoverflow.js
+++ server/src/Stackoverflow.js
@@ -1,20 +1,17 @@
 'use strict';
 
-const https = require('https');
+const CodingSite = require('./CodingSite');
 const {URL} = require('url');
 const util = require('util');
-const zlib = require('zlib');
 
 // Uses a custom filter that only returns backoff, quota_remaining, and total
 // (https://api.stackexchange.com/docs/create-filter#unsafe=false&filter=!.UE8F0bVg4M-_Ii4&run=true)
 const API_URL = 'https://api.stackexchange.com/2.2/search?todate=%s&site=stackoverflow&tagged=%s&filter=!.UE8F0bVg4M-_Ii4';
 
-module.exports = class Stackoverflow {
+module.exports = class Stackoverflow extends CodingSite {
   async getScore(langName, date) {
     let url = this._buildUrl(date, langName);
-
-    let bodyJson = await this._httpsRequest(url);
-    let body = JSON.parse(bodyJson);
+    let body = await this._callApi(url);
 
     // TODO: find and use a proper logging framework
     console.log(`DEBUG: StackOverflow API daily quota remaining: ${body.quota_remaining}`);
@@ -27,10 +24,6 @@ module.exports = class Stackoverflow {
     }
 
     return body.total;
-  }
-
-  set apiKey(newApiKey) {
-    this._apiKey = newApiKey;
   }
 
   _buildUrl(date, langName) {
@@ -50,6 +43,13 @@ module.exports = class Stackoverflow {
     return encodeURIComponent(langName.toLowerCase().replace(/ /g, '-'));
   }
 
+  async _callApi(url) {
+    const options = new URL(url);
+
+    let bodyJson = await CodingSite._httpsRequest(options);
+    return JSON.parse(bodyJson);
+  }
+
   _addApiKey(url) {
     const KEY_PARAMETER = '&key=';
     if (typeof this._apiKey !== 'undefined') {
@@ -57,51 +57,5 @@ module.exports = class Stackoverflow {
     }
 
     return url;
-  }
-
-  // TODO: clean this up and make it less generic?
-  //  - we don't need postData
-  //  - we don't need to handle uncompressed body
-  //  - we can parse the JSON here
-  // Based on https://stackoverflow.com/a/38543075/399105
-  _httpsRequest(url, postData) {
-    const options = new URL(url);
-
-    return new Promise(function(resolve, reject) {
-      let request = https.request(options, function(response) {
-        // Reject on bad status code
-        if (response.statusCode < 200 || response.statusCode >= 300) {
-          return reject(new Error('statusCode=' + response.statusCode));
-        }
-        let body = [];
-        response.on('data', function(chunk) {
-          body.push(chunk);
-        });
-        response.on('end', function() {
-          try {
-            switch (response.headers['content-encoding']) {
-              case 'gzip':
-                zlib.gunzip(Buffer.concat(body), (error, uncompressedData) => {
-                  resolve(uncompressedData.toString());
-                });
-                break;
-              default:
-                resolve(Buffer.concat(body).toString());
-                break;
-            }
-          } catch (e) {
-            reject(e);
-          }
-        });
-      });
-      // Reject on request error
-      request.on('error', function(err) {
-        reject(err);
-      });
-      if (postData) {
-        request.write(postData);
-      }
-      request.end();
-    });
   }
 };
