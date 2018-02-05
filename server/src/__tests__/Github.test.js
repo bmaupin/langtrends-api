9e414330f8753e6a0a7c136534bd35e1cc6b1c43
bmaupin <bmaupin@users.noreply.github.com>
2018-02-05 13:58:40 -0500

    Don't encode post data for github

--- server/src/__tests__/Github.test.js
+++ server/src/__tests__/Github.test.js
@@ -13,5 +13,6 @@ test('Test getScore', async () => {
   if (process.env.hasOwnProperty('GITHUB_API_KEY')) {
     github.apiKey = process.env.GITHUB_API_KEY;
   }
-  expect(await github.getScore('JavaScript', new Date('2017-01-01'))).toBeGreaterThan(1000000);
+  expect(await github.getScore('C++', new Date('2017-01-01'))).toBeGreaterThan(100000);
+  expect(await github.getScore('C++', new Date('2017-01-01'))).toBeLessThan(10000000);
 });
