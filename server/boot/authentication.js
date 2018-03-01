d218cb7f7d83b59cc664ddc8d484b7b56e1e04d4
bmaupin <bmaupin@users.noreply.github.com>
2018-03-01 12:36:39 -0500

    Add basic auth support

---
+++ server/boot/authentication.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = function enableAuthentication(server) {
+  // enable authentication
+  // TODO: enable this (requires creating at least one user)
+  //server.enableAuth();
+};
