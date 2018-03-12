783050906a003ba2165df9d159cacb7540cb9742
bmaupin <bmaupin@users.noreply.github.com>
2018-03-12 18:15:52 +0000

    Configure loopback for https

--- server/server.js
+++ server/server.js
@@ -3,19 +3,36 @@
 var loopback = require('loopback');
 var boot = require('loopback-boot');
 
+var http = require('http');
+var https = require('https');
+var sslConfig = require('./ssl-config');
+
 var app = module.exports = loopback();
 
-app.start = function() {
-  // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
+app.start = function(httpOnly) {
+  if (httpOnly === undefined) {
+    httpOnly = process.env.HTTP;
+  }
+  var server = null;
+  if (!httpOnly) {
+    var options = {
+      key: sslConfig.privateKey,
+      cert: sslConfig.certificate,
+    };
+    server = https.createServer(options, app);
+  } else {
+    server = http.createServer(app);
+  }
+  server.listen(app.get('port'), function() {
+    var baseUrl = (httpOnly ? 'http://' : 'https://') + app.get('host') + ':' + app.get('port');
+    app.emit('started', baseUrl);
+    console.log('LoopBack server listening @ %s%s', baseUrl, '/');
     if (app.get('loopback-component-explorer')) {
       var explorerPath = app.get('loopback-component-explorer').mountPath;
       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
     }
   });
+  return server;
 };
 
 // Bootstrap the application, configure models, datasources and middleware.
