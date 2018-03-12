e252c8ac8c2c4d7fc2ab8d770260d9dc8387c126
bmaupin <bmaupin@users.noreply.github.com>
2018-03-12 18:15:52 +0000

    Configure loopback for https

---
+++ server/ssl-config.js
@@ -0,0 +1,5 @@
+var path = require('path');
+var fs = require('fs');
+
+exports.privateKey = fs.readFileSync(path.join(__dirname, './private/privkey.pem')).toString();
+exports.certificate = fs.readFileSync(path.join(__dirname, './private/fullchain.pem')).toString();
