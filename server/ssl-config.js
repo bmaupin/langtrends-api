65d67ead60268ffd6666a5ecee77c7fb51cdb209
bmaupin <bmaupin@users.noreply.github.com>
2018-03-21 13:15:31 -0400

    Revert SSL and CORS configuration
    OpenShift seems to handle HTTPS for us. This reverts commits 46b95fe, 7adcbdc, e3ec587

--- server/ssl-config.js
+++
@@ -1,5 +0,0 @@
-var path = require('path');
-var fs = require('fs');
-
-exports.privateKey = fs.readFileSync(path.join(__dirname, './private/privkey.pem')).toString();
-exports.certificate = fs.readFileSync(path.join(__dirname, './private/fullchain.pem')).toString();
