b2fc6424cc697f539916abfd0893a58397db4140
bmaupin <bmaupin@users.noreply.github.com>
2018-04-03 14:14:28 -0400

    Schedule populateData to be run daily

--- server/boot/populate-data.js
+++ server/boot/populate-data.js
@@ -3,9 +3,21 @@
 const DataPopulator = require('../src/DataPopulator');
 
 module.exports = function(app, cb) {
+  populateData(app, cb);
+  schedulePopulateDataDaily(app);
+};
+
+function schedulePopulateDataDaily(app) {
+  const MILLISECONDS_PER_DAY = 86400000;
+  setInterval(populateData, MILLISECONDS_PER_DAY, app);
+}
+
+function populateData(app, cb) {
+  console.log(`DEBUG: ${new Date(Date.now()).toISOString()}\tpopulateData`);
+
   let dataPopulator = new DataPopulator(app, cb);
 
   dataPopulator.populateAllLanguages().then(() => {
     dataPopulator.populateTopScores();
   });
-};
+}
