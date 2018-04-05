fb83334679137fa4de28d2e522f85776641d8685
bmaupin <bmaupin@users.noreply.github.com>
2018-04-05 12:42:59 -0400

    Make sure database schema is created before populating data

--- server/boot/create-database-schema.js
+++ server/boot/create-database-schema.js
@@ -3,18 +3,35 @@
 // Create database schema if it doesn't exist (https://loopback.io/doc/en/lb3/Creating-a-database-schema-from-models.html)
 // This is effectively a no-op for the memory connector (https://loopback.io/doc/en/lb3/Memory-connector.html)
 // Based on https://stackoverflow.com/a/40032444/399105
-module.exports = function(app) {
-  let ds = app.datasources.db;
-
-  for (let model in app.models) {
-    ds.isActual(model, (err, actual) => {
-      if (err) throw err;
-      if (!actual) {
-        ds.autoupdate(model, (err, result) => {
-          if (err) throw err;
-          console.log(`Autoupdate performed for model ${model}`);
-        });
-      }
-    });
-  }
+module.exports = function(app, cb) {
+  createDatabaseSchema(app).then(() => {
+    process.nextTick(cb);
+  });
 };
+
+function createDatabaseSchema(app) {
+  return new Promise((resolve, reject) => {
+    let datastore = app.datasources.db;
+
+    for (let model in app.models) {
+      datastore.isActual(model, async (err, actual) => {
+        if (err) reject(err);
+        if (!actual) {
+          await autoupdate(datastore, model);
+        }
+      });
+    }
+
+    resolve();
+  });
+}
+
+function autoupdate(datastore, model) {
+  return new Promise((resolve, reject) => {
+    datastore.autoupdate(model, (err, result) => {
+      if (err) reject(err);
+      console.log(`Autoupdate performed for model ${model}`);
+      resolve();
+    });
+  });
+}
