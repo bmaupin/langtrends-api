c3f98451cacd3046045e222fe33267d5afd914f2
bmaupin <bmaupin@users.noreply.github.com>
2018-04-18 13:52:22 -0400

    Get all scores for all languages

--- server/boot/03-populate-data.js
+++ server/boot/03-populate-data.js
@@ -19,7 +19,7 @@ async function populateData(app) {
 
   try {
     await dataPopulator.populateAllLanguages();
-    await dataPopulator.populateTopScores();
+    await dataPopulator.populateAllScores();
   } catch (error) {
     // Don't abort on errors
     console.error(error);
