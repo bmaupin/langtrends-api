3a62b523d348a4ac5f28d687e56efed45e07e35f
bmaupin <bmaupin@users.noreply.github.com>
2019-02-28 14:14:07 -0500

    Migrate from PostgreSQL to MongoDB :'(
    Free OpenShift Online accounts expire every 60 days now, deleting all data, so we need a better option in order to avoid having to recreate everything every 60 days...

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -2,11 +2,8 @@
 
 module.exports = {
   db: {
-    connector: 'postgresql',
-    host: process.env.POSTGRESQL_SERVICE_HOST || 'localhost',
-    port: process.env.POSTGRESQL_SERVICE_PORT || 5432,
-    user: process.env.POSTGRESQL_USER,
-    password: process.env.POSTGRESQL_PASSWORD,
-    database: process.env.POSTGRESQL_DATABASE,
+    connector: 'mongodb',
+    url: process.env.DATABASE_URL,
+    useNewUrlParser: true,
   },
 };
