946a534c0b9096624ddab8a4fedd40bbd8a3762c
bmaupin <bmaupin@users.noreply.github.com>
2020-01-28 13:16:16 -0500

    Migrate back to PostgreSQL from MongoDB 🎉
    Fixes #1

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -2,7 +2,13 @@
 
 module.exports = {
   db: {
-    connector: 'mongodb',
+    connector: 'postgresql',
+    // Scalingo only allows max 10 DB connections, only 8 are available to us, and the pgAdmin client can use several.
+    // Using too many connections throws error: remaining connection slots are reserved for non-replication superuser
+    // connections
+    max: 5,
+    // Scalingo requires SSL
+    ssl: true,
     url: process.env.DATABASE_URL,
     useNewUrlParser: true,
   },
