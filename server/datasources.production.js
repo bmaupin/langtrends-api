d0b154a7fe9fa3b7f43de6b6ad872f0ddb93cf30
bmaupin <bmaupin@users.noreply.github.com>
2020-02-17 13:17:18 -0500

    Migrate from Scalingo postgres to Clever Cloud
    Scalingo requires a payment method after the trial expires

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -3,12 +3,9 @@
 module.exports = {
   db: {
     connector: 'postgresql',
-    // Scalingo only allows max 10 DB connections, only 8 are available to us, and the pgAdmin client can use several.
-    // Using too many connections throws error: remaining connection slots are reserved for non-replication superuser
-    // connections
-    max: 5,
-    // Scalingo requires SSL
-    ssl: true,
+    // Clever Cloud only allows max 5 DB connections and the pgAdmin client can use several. Using too many connections
+    // throws error: too many connections for role
+    max: 2,
     url: process.env.DATABASE_URL,
     useNewUrlParser: true,
   },
